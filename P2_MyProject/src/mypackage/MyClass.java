706bd06722867cdb381402c1fe89e923ccdb0ec0
Sivaiah Gunda <sivaiah.gunda@signanthealth.com>
2022-04-11 00:07:19 +0530

    Sample programs

---
+++ P2_MyProject/src/mypackage/MyClass.java
@@ -0,0 +1,17 @@
+package mypackage;
+
+public class MyClass {
+
+	public static void main(String[] args) 
+	{
+		int a = 50;
+		
+		System.out.println(a++);//post increment
+		System.out.println(++a);//pre-increment
+		System.out.println(a--);//post decrement
+		System.out.println(--a);//pre-decrement
+	
+
+	}
+
+}
